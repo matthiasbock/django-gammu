ec1451f0b1dc37bdff83f21f919989739e138128
matthias.bock@hu-berlin.de <matthias.bock@hu-berlin.de@5232c3b4-0cb1-746d-dff9-a0945efc20fe>
2011-09-20 17:13:48 +0000

    calendar

--- trunk/gammu/models.py
+++ trunk/gammu/models.py
@@ -9,6 +9,23 @@
 
 from django.db import models
 
+class Contactatoms(models.Model):
+    id = models.IntegerField(primary_key=True, db_column='ID') # Field name made lowercase.
+    contact = models.IntegerField(db_column='Contact') # Field name made lowercase.
+    property = models.CharField(max_length=150, db_column='Property') # Field name made lowercase.
+    value = models.CharField(max_length=180, db_column='Value') # Field name made lowercase.
+    comment = models.CharField(max_length=300, db_column='Comment') # Field name made lowercase.
+    class Meta:
+        db_table = u'ContactAtoms'
+
+class Contacts(models.Model):
+    id = models.IntegerField(primary_key=True, db_column='ID') # Field name made lowercase.
+    owner = models.IntegerField(db_column='Owner') # Field name made lowercase.
+    firstname = models.CharField(max_length=120, db_column='Firstname') # Field name made lowercase.
+    surname = models.CharField(max_length=120, db_column='Surname') # Field name made lowercase.
+    class Meta:
+        db_table = u'Contacts'
+
 class Events(models.Model):
     id = models.IntegerField(primary_key=True, db_column='ID') # Field name made lowercase.
     start = models.DateTimeField(null=True, db_column='Start', blank=True) # Field name made lowercase.
@@ -16,4 +33,18 @@ class Events(models.Model):
     summary = models.CharField(max_length=765, db_column='Summary', blank=True) # Field name made lowercase.
     class Meta:
         db_table = u'Events'
+
+class Nicknametable(models.Model):
+    id = models.IntegerField(primary_key=True, db_column='ID') # Field name made lowercase.
+    nick = models.CharField(max_length=300, db_column='Nick') # Field name made lowercase.
+    real = models.CharField(max_length=300, db_column='Real') # Field name made lowercase.
+    class Meta:
+        db_table = u'NicknameTable'
+
+class Users(models.Model):
+    id = models.IntegerField(primary_key=True, db_column='ID') # Field name made lowercase.
+    username = models.CharField(max_length=150, db_column='Username') # Field name made lowercase.
+    passwordhash = models.CharField(max_length=96, db_column='PasswordHash') # Field name made lowercase.
+    class Meta:
+        db_table = u'Users'
 
