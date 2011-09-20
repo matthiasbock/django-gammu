82590d5fd93bcd6201adfb885fa6b03f030b5e1b
matthias.bock <matthias.bock@hu-berlin.de>
2011-09-20 18:17:52 +0000

    simplelist

--- urls.py
+++ urls.py
@@ -13,9 +13,11 @@ urlpatterns = patterns('',
 	(r'^contacts/static/(?P<path>.*)$',		'django.views.static.serve', {'document_root': '/var/www/Django/gammu/static', 'show_indexes': True}),
 
 	(r'^calendar/$',				"Django.gammu.calendar.index"),
+	(r'^calendar/simplelist$',			"Django.gammu.calendar.simplelist"),
 	(r'^calendar/listing$',				"Django.gammu.calendar.listing"),
 	(r'^calendar/week$',				"Django.gammu.calendar.week"),
 	(r'^calendar/month$',				"Django.gammu.calendar.month"),
+	(r'^calendar/save$',				"Django.gammu.calendar.save"),
 
 	(r'^calendar/import/$',				"Django.gammu.calendar.import.index"),
 	(r'^calendar/export/$',				"Django.gammu.calendar.export.index"),
