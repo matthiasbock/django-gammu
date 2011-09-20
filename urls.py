efb0d4e7a696684799c1f6b6fb641893ff815460
matthias.bock <matthias.bock@hu-berlin.de>
2011-09-20 17:13:48 +0000

    calendar

--- urls.py
+++ urls.py
@@ -1,16 +1,16 @@
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
-	(r'^contacts/$',				"Django.contacts.main.index"),
-	(r'^contacts/contacts$',			"Django.contacts.main.Table"),
-	(r'^contacts/edit$',				"Django.contacts.main.EditContact"),
-	(r'^contacts/remove$',				"Django.contacts.main.RemoveContact"),
-	(r'^contacts/addatom$',				"Django.contacts.main.AddAtom"),
-	(r'^contacts/removeatom$',			"Django.contacts.main.RemoveAtom"),
-	(r'^contacts/synchronize$',			"Django.contacts.sync.index"),
-	(r'^contacts/IrDA/attach$',			"Django.contacts.irda.attach"),
-	(r'^contacts/IrDA/download$',			"Django.contacts.irda.download"),
-	(r'^contacts/static/(?P<path>.*)$',		'django.views.static.serve', {'document_root': '/var/www/Django/contacts/static', 'show_indexes': True}),
+	(r'^contacts/$',				"Django.gammu.main.index"),
+	(r'^contacts/contacts$',			"Django.gammu.contacts.Table"),
+	(r'^contacts/edit$',				"Django.gammu.contacts.EditContact"),
+	(r'^contacts/remove$',				"Django.gammu.contacts.RemoveContact"),
+	(r'^contacts/addatom$',				"Django.gammu.contacts.AddAtom"),
+	(r'^contacts/removeatom$',			"Django.gammu.contacts.RemoveAtom"),
+	(r'^contacts/synchronize$',			"Django.gammu.contacts.sync"),
+	(r'^contacts/IrDA/attach$',			"Django.gammu.contacts.irdaattach"),
+	(r'^contacts/IrDA/download$',			"Django.gammu.contacts.irdadownload"),
+	(r'^contacts/static/(?P<path>.*)$',		'django.views.static.serve', {'document_root': '/var/www/Django/gammu/static', 'show_indexes': True}),
 
 	(r'^calendar/$',				"Django.gammu.calendar.index"),
 	(r'^calendar/listing$',				"Django.gammu.calendar.listing"),
@@ -19,6 +19,6 @@ urlpatterns = patterns('',
 
 	(r'^calendar/import/$',				"Django.gammu.calendar.import.index"),
 	(r'^calendar/export/$',				"Django.gammu.calendar.export.index"),
-	(r'^calendar/static/(?P<path>.*)$',		'django.views.static.serve', {'document_root': '/var/www/Django/calendar/static', 'show_indexes': True}),
+	(r'^calendar/static/(?P<path>.*)$',		'django.views.static.serve', {'document_root': '/var/www/Django/gammu/static', 'show_indexes': True}),
 )
 
